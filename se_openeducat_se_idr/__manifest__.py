db551e8ca65222886f0174249cd4d74e53962fa6
aaparicio87 <dlaparicio87@gmail.com>
2021-05-17 22:04:57 -0400

    [ADD Adde new Fields to student view

--- se_openeducat_se_idr/__manifest__.py
+++ se_openeducat_se_idr/__manifest__.py
@@ -11,19 +11,15 @@
     """,
 
     'author': "Alejandro",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Uncategorized',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','openeducat_core',],
 
     # always loaded
     'data': [
         'security/ir.model.access.csv',
-        'views/views.xml',
+        'views/op_student.xml',
     ],
 }
