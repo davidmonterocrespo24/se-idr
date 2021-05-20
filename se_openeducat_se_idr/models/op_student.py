b74eb2fd434e4cc7d35a3be0bb55da39c1955952
dmontero <dmontero@quanam.com>
2021-05-20 11:05:04 -0300

    contrain unico matricula

--- se_openeducat_se_idr/models/op_student.py
+++ se_openeducat_se_idr/models/op_student.py
@@ -14,11 +14,14 @@ class OpStudent(models.Model):
     institutional_email = fields.Char('Email Institucional', size=256)
     enrollment_number = fields.Char(string='Número de Matrícula', size=10)
 
+
+    _sql_constraints = [
+        ('unique_enrollment_number', 'unique (enrollment_number)', 'Número de Matrícula no puede estar repetido!')
+    ]
+
     @api.constrains('enrollment_number')
     def check_number_digits(self):
         for record in self:
             if len(record.enrollment_number) < 10:
                 raise ValidationError("Número de Matrícula no puede contener menos de 10 números")
-    
-            
-    
+
