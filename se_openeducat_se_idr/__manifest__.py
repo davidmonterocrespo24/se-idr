7e1b04487071f84c2754c2817052e326c00d85d3
Jose Andres Hernandez Bustio <jbustio@gmail.com>
2021-05-25 15:15:58 -0400

    fix discount

--- se_openeducat_se_idr/__manifest__.py
+++ se_openeducat_se_idr/__manifest__.py
@@ -15,11 +15,12 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','openeducat_core',],
+    'depends': ['base','openeducat_core','openeducat_fees'],
 
     # always loaded
     'data': [
         'security/ir.model.access.csv',
         'views/op_student.xml',
+        'views/student_view.xml',
     ],
 }
