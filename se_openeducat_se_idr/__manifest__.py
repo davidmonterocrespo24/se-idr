8f76b429e9d3e98316797bcd1983d1ab76cc731f
dmontero <dmontero@quanam.com>
2021-05-27 00:30:01 -0300

    comprobante de pago

--- se_openeducat_se_idr/__manifest__.py
+++ se_openeducat_se_idr/__manifest__.py
@@ -14,13 +14,12 @@
     'category': 'Uncategorized',
     'version': '0.1',
 
-    # any module necessary for this one to work correctly
     'depends': ['base','openeducat_core','openeducat_fees'],
 
-    # always loaded
     'data': [
         'security/ir.model.access.csv',
         'views/op_student.xml',
-        #'views/student_view.xml',
+        'views/student_view.xml',
+        'views/account_payment_view'
     ],
 }
--- se_openeducat_se_idr/models/__init__.py
+++ se_openeducat_se_idr/models/__init__.py
@@ -2,3 +2,4 @@
 
 from . import op_student
 from . import op_admission
+from .  import account_invoice
