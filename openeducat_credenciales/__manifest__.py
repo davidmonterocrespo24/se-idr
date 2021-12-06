11538c53bf2bc8fc73e2f0428abaeeda281d87ac
Raul Rolando Jardinot Gonzalez <rauljardinot@gmail.com>
2021-12-06 21:01:21 +0100

    Update
    CORREGIDO ERRORES Y ELIMINADO TODOS LOS ARCHIVOS QUE NO SE USAN
    
    6/12/2021

--- openeducat_credenciales/__manifest__.py
+++ openeducat_credenciales/__manifest__.py
@@ -11,25 +11,17 @@
 
     'author': "Raul Rolando Jardinot Gonzalez",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
-    # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '13.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'openeducat_erp', 'hr_employee_se_idr'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/card_frontal_ok.xml',
-        'views/card_trasera_ok.xml',
+        'views/card_frontal.xml',
+        'views/card_trasera.xml',
         'views/op_category_inherit.xml',
         'views/op_student_inherit.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
     ],
 }
