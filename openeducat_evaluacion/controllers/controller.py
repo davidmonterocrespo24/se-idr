792f2ff05d6c375728996861b5cb55a38d1a5a62
Raul Rolando Jardinot Gonzalez <49165764+rauljardinot@users.noreply.github.com>
2022-01-06 13:52:24 -0500

    añadido controlador
    6/01/2022
    
    13:52

---
+++ openeducat_evaluacion/controllers/controller.py
@@ -0,0 +1,20 @@
+# -- coding: utf-8 --
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import fields, http, _
+from odoo.exceptions import AccessError, MissingError
+from odoo.http import request
+from odoo.addons.payment.controllers.portal import PaymentProcessing
+from odoo.addons.portal.controllers.mail import _message_post_helper
+from odoo.addons.portal.controllers.portal import CustomerPortal, pager as portal_pager, get_records_pager
+
+
+class CustomerPortal(CustomerPortal):
+
+    @http.route(['/credencial'], type='http', auth="user", website=True)
+    def home(self, **kw):
+        values.update({
+            'docs': request.user_id.user_line,
+        })
+
+        return request.render("openeducat_credenciales.report_credencial_template", values)
